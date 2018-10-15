28f67e636a824daa645a910d1f8c81fec2ac5630
Jesper Ahasverusen Kielsgaard <jkielsgaard@outlook.com>
2018-10-15 21:48:15 +0200

    add changepassword webapi function

--- ClimateViewer/Handlers/HttpApiRequest.cs
+++ ClimateViewer/Handlers/HttpApiRequest.cs
@@ -40,8 +40,37 @@ namespace ClimateViewer.Handlers
             byte[] bodydata = Encoding.ASCII.GetBytes(body);
 
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(dataurl + urlparams);
-            request.Method = "post";
+            request.Method = "POST";
             request.ContentType = "application/json";          
+            request.ContentLength = bodydata.Length;
+
+            Stream newStream = request.GetRequestStream();
+            newStream.Write(bodydata, 0, bodydata.Length);
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            string JSONapikey = "";
+            using (StreamReader sr = new StreamReader(response.GetResponseStream())) { JSONapikey = sr.ReadToEnd(); }
+
+            if (JSONapikey == "[]") { return null; }
+            else { return JSONapikey; }
+        }
+
+        public static string ChangePassword(string apikey, string usermail, string newpassword)
+        {
+            ChangePassword cp = new ChangePassword();
+            cp.usermailid = usermail;
+            cp.newpassword = newpassword;
+
+            string dataurl = "https://gab8d2upqj.execute-api.eu-west-1.amazonaws.com/dev/climateapi";
+            string urlparams = "?httpFunction=changepassword";
+            string body = JsonConvert.SerializeObject(cp);
+
+            byte[] bodydata = Encoding.ASCII.GetBytes(body);
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(dataurl + urlparams);
+            request.Method = "PUT";
+            request.Headers.Add("x-api-key", apikey);
+            request.ContentType = "application/json";
             request.ContentLength = bodydata.Length;
 
             Stream newStream = request.GetRequestStream();
@@ -68,7 +97,40 @@ namespace ClimateViewer.Handlers
             byte[] bodydata = Encoding.ASCII.GetBytes(body);
 
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(dataurl + urlparams);
-            request.Method = "post";
+            request.Method = "POST";
+            request.Headers.Add("x-api-key", apikey);
+            request.ContentType = "application/json";
+            request.ContentLength = bodydata.Length;
+
+            Stream newStream = request.GetRequestStream();
+            newStream.Write(bodydata, 0, bodydata.Length);
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            string Response = "";
+            using (StreamReader sr = new StreamReader(response.GetResponseStream())) { Response = sr.ReadToEnd(); }
+            return Response;
+        }
+
+        public static string Changeunits(string apikey, string usermail, List<Userunits> units)
+        {
+            string[] unitsarray = new string[units.Count()];
+            for (int i = 0; i < units.Count(); i++)
+            {
+                unitsarray[i] = units[i].id + "," + units[i].name;
+            }
+
+            ChangeUnits cu = new ChangeUnits();
+            cu.usermailid = usermail;
+            cu.units = unitsarray;
+
+            string dataurl = "https://gab8d2upqj.execute-api.eu-west-1.amazonaws.com/dev/climateapi";
+            string urlparams = "?httpFunction=units";
+            string body = JsonConvert.SerializeObject(cu);
+
+            byte[] bodydata = Encoding.ASCII.GetBytes(body);
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(dataurl + urlparams);
+            request.Method = "PUT";
             request.Headers.Add("x-api-key", apikey);
             request.ContentType = "application/json";
             request.ContentLength = bodydata.Length;
