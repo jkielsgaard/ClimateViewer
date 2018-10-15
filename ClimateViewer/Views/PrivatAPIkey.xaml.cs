bac69fb158ee6755de40c8594e660fe1e9c9ca1c
Jesper Ahasverusen Kielsgaard <jkielsgaard@outlook.com>
2018-10-15 21:49:09 +0200

    User can now get privat apikey when loged in

---
+++ ClimateViewer/Views/PrivatAPIkey.xaml.cs
@@ -0,0 +1,45 @@
+﻿using ClimateViewer.Handlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ClimateViewer.Views
+{
+    /// <summary>
+    /// Interaction logic for PrivatAPIkey.xaml
+    /// </summary>
+    public partial class PrivatAPIkey : Window
+    {
+        public PrivatAPIkey()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            lb_apikey.Content = "Privat API key: " + UserInformation.ApiKey;
+        }
+
+        private void btn_copy_Click(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(UserInformation.ApiKey);
+        }
+
+        private void btn_close_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+ 
+    }
+}
